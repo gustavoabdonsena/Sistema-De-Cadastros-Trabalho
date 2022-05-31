d3860a630da9412394267b57b688ebaebcba6a2b
Gustavo Abdon <45674528+gustavoabdonsena@users.noreply.github.com>
2022-05-31 15:12:06 -0300

    base

--- src/main/java/app/database/FileStorage.java
+++ src/main/java/app/database/FileStorage.java
@@ -1,7 +1,14 @@
 package app.database;
-
+//This class will be designated to load TXT as database
 public class FileStorage {
 
 
+    public static void loadList(){
+
+    }
+
+    public static void unloadList(){
+
+    }
 
 }
--- src/main/java/app/database/UsuariosList.java
+++ src/main/java/app/database/UsuariosList.java
@@ -8,9 +8,20 @@ public class UsuariosList {
 
     static ArrayList<Funcionario> funcionarios = new ArrayList<>();
 
+    //this method will add new Employees to the array
     public static void addFuncionario(Funcionario funcionario){
        // funcionarios.add(funcionario);
         System.out.println("Funcionário adicionado com sucesso!");
+        //call method to reload the txt file with new clients
+
+    }
+
+    //este método receberá os funcionários da lista
+    public static boolean loadFuncionariosList(){
+
+
+
+        return true;
     }
 
 }
