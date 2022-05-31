62ffa7e7aa19bcc0afead6f4ee17e25a80acf47f
Gustavo Abdon <45674528+gustavoabdonsena@users.noreply.github.com>
2022-05-31 17:12:01 -0300

    a

--- src/main/java/app/database/FileStorage.java
+++ src/main/java/app/database/FileStorage.java
@@ -1,10 +1,29 @@
 package app.database;
+
+import java.io.File;
+import java.io.IOException;
+
 //This class will be designated to load TXT as database
 public class FileStorage {
 
 
-    public static void loadList(){
 
+    private static void createFile(){
+        try{
+            File database = new File("database.txt");
+            if(database.createNewFile()){
+              System.out.println("Arquivo criado");
+            }else {
+                System.out.println("erro");
+            }
+        }
+        catch (IOException e){
+            System.out.println("Ocorreu um erro na criação do arquivo! ");
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadList(){
     }
 
     public static void unloadList(){
