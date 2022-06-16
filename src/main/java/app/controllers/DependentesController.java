852e621e5c777889df3b8576c2db7b3c740561c7
LariST17 <larastrece@gmail.com>
2022-06-16 14:01:34 -0300

    16/06

--- src/main/java/app/controllers/DependentesController.java
+++ src/main/java/app/controllers/DependentesController.java
@@ -2,13 +2,13 @@ package app.controllers;
 
 import app.Main;
 import app.client.Dependente;
-import app.database.FileJSONWrite;
 import app.database.UsuariosList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -22,21 +22,28 @@ public class DependentesController {
     private Label prompt;
     @FXML
     private TextField dependentName;
+    @FXML
+    private Button add;
+    @FXML
+    private Button next;
 
     private ArrayList<Dependente> dependentes = new ArrayList<>();
 
 
     @FXML
-    void addDependente(ActionEvent event) throws IOException
+    void addDependente()
     {
-
-        Dependente dependente = new Dependente(dependentName.getText());
-        dependentes.add(dependente);
-        prompt.setText("Dependente adicionado");
-
-        UsuariosList.getFuncionarios().get(-1).setDependentesList(dependentes);
-
-        FileJSONWrite.createJSON();
+        if (dependentes.size() == UsuariosList.getFuncionarios().get(-1).getDependentes())
+        {
+            UsuariosList.getFuncionarios().get(-1).setDependentesList(dependentes);
+            add.setDisable(true);
+            next.setVisible(true);
+        }
+        else
+        {
+            dependentes.add(new Dependente(dependentName.getText()));
+            prompt.setText("Dependente adicionado");
+        }
     }
 
     @FXML
@@ -51,7 +58,6 @@ public class DependentesController {
 
     @FXML
     void next(ActionEvent event) throws IOException {
-        //Switch to employee's info page
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("funcionarios.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 450);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -59,4 +65,5 @@ public class DependentesController {
         stage.setScene(scene);
         stage.show();
     }
+
 }
