7e209afd5b818949563b092208e2271f948c5afb
LariST17 <larastrece@gmail.com>
2022-06-16 12:28:47 -0300

    16/06

--- src/main/java/app/controllers/DependentesController.java
+++ src/main/java/app/controllers/DependentesController.java
@@ -1,54 +1,58 @@
 package app.controllers;
 
 import app.Main;
+import app.client.Dependente;
+import app.database.FileJSONWrite;
+import app.database.UsuariosList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DependentesController {
 
     @FXML
     private Label prompt;
+    @FXML
+    private TextField dependentName;
+
+    private ArrayList<Dependente> dependentes = new ArrayList<>();
+
 
     @FXML
-    private VBox vbox;
-
-    @FXML
-    void addDependentes(ActionEvent event) {
-
-    }
-
-    /*@FXML
-    void addDependentes(ActionEvent event) throws IOException
+    void addDependente(ActionEvent event) throws IOException
     {
-        ArrayList<Dependente> dependentes = new ArrayList<>();
-        for (int i = 0; i < Integer.parseInt(inputQtdDependentes.getText()); i++)
-        {
-            TextField inputDependente = (TextField) scene.lookup("#inputDependente" + i);
-            Dependente dependente = new Dependente(inputDependente.getText());
-            dependentes.add(dependente);
-        }
+
+        Dependente dependente = new Dependente(dependentName.getText());
+        dependentes.add(dependente);
+        prompt.setText("Dependente adicionado");
+
         UsuariosList.getFuncionarios().get(-1).setDependentesList(dependentes);
 
-        //Switch to employee's info page
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("funcionarios.fxml"));
-        scene = new Scene(fxmlLoader.load(), 800 , 450);
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setTitle("Sistemas de Funcionários");
-        stage.setScene(scene);
-        stage.show();
-    }*/
+        FileJSONWrite.createJSON();
+    }
 
     @FXML
     void backToFirst(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("first-scene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 450);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Sistemas de Funcionários");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void next(ActionEvent event) throws IOException {
+        //Switch to employee's info page
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("funcionarios.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 450);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle("Sistemas de Funcionários");
